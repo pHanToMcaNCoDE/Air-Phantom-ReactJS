0186144b77649bf41508f93f180549578dab94a2
Victory Odumeh <vodumeh@gmail.com>
2023-08-20 00:28:40 +0100

    working on some minor issues

--- flightBooking/src/App.jsx
+++ flightBooking/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/Home'
-import Search from './Components/Search/Search'
-import Support from './Components/Support/Support'
-import Info from './Components/Info/Info'
-import Lounge from './Components/Lounge/Lounge'
-import Travelers from './Components/Travelers/Travelers'
-import Subscribers from './Components/Subscribers/Subscribers'
-import Footer from './Components/Footer/Footer'
+// import Home from './Components/Home/Home'
+// import Search from './Components/Search/Search'
+// import Support from './Components/Support/Support'
+// import Info from './Components/Info/Info'
+// import Lounge from './Components/Lounge/Lounge'
+// import Travelers from './Components/Travelers/Travelers'
+// import Subscribers from './Components/Subscribers/Subscribers'
+// import Footer from './Components/Footer/Footer'
+
 
 const App = () => {
   return (
@@ -26,3 +27,4 @@ const App = () => {
 }
 
 export default App
+
